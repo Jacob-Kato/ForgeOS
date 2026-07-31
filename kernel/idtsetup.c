93d4317ed35c9c51a4c733cb4f3efa799e10129c
Jacob-Kato <jacobyala23@gmail.com>
2026-07-31 10:45:33 -0400

    Define a struct to match the saved state of the cpu

--- kernel/idtsetup.c
+++ kernel/idtsetup.c
@@ -11,6 +11,25 @@ struct idt_entry {
   uint32_t reserved;
 };
 
+#include <stdint.h>
+
+struct interrupt_frame {
+  uint64_t r15;
+  uint64_t r14;
+  uint64_t r13;
+  uint64_t r12;
+  uint64_t r11;
+  uint64_t r10;
+  uint64_t r9;
+  uint64_t r8;
+  uint64_t rbp;
+  uint64_t rdi;
+  uint64_t rsi;
+  uint64_t rdx;
+  uint64_t rcx;
+  uint64_t rbx;
+  uint64_t rax;
+};
 extern struct idt_entry idt_table[256];
 extern void *isr_stub_table[256];
 
